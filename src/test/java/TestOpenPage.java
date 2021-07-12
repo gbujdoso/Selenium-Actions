a39e79420e9b0182700d488c045148f69323bcf0
master <gbujdoso>
2021-07-12 23:27:11 +0200

    first commit

--- src/test/java/TestOpenPage.java
+++ src/test/java/TestOpenPage.java
@@ -1,9 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class TestOpenPage {
 
     @Test
     public void test() {
-        OpenPage.main(null);
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+
+        driver.get("http://hwsw.hu");
     }
 }
