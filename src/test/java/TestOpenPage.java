a5fffe7b504e76695ec0d96a01577fc449ef38f0
master <gbujdoso>
2021-07-13 10:33:47 +0200

    first commit

--- src/test/java/TestOpenPage.java
+++ src/test/java/TestOpenPage.java
@@ -23,7 +23,7 @@ public class TestOpenPage {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
